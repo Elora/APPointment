ff6090f3ca09c47664b8cacd4d1040cd78199852
Tony Kwan <tonykwan93@gmail.com>
2014-12-19 11:25:48 +0100

    first commit

---
+++ src/kb50/appointment/LogIn.java
@@ -0,0 +1,15 @@
+package kb50.appointment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class LogIn extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
