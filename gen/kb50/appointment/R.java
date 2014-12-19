16ce9e1f1ada14cb294074bc347f6922db146dd3
Tony Kwan <tonykwan93@gmail.com>
2014-12-19 13:30:13 +0100

    We have tabs

--- gen/kb50/appointment/R.java
+++ gen/kb50/appointment/R.java
@@ -13,12 +13,27 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int loginBtn=0x7f060002;
+        public static final int passwordField=0x7f060001;
+        public static final int realtabcontent=0x7f060005;
+        public static final int regText=0x7f060003;
+        public static final int registerBtn=0x7f060004;
+        public static final int usernameField=0x7f060000;
+    }
     public static final class layout {
         public static final int activity_log_in=0x7f030000;
+        public static final int appointment_list_layout=0x7f030001;
+        public static final int profile_layout=0x7f030002;
+        public static final int tab_layout=0x7f030003;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int login=0x7f040003;
+        public static final int password=0x7f040002;
+        public static final int regText=0x7f040005;
+        public static final int register=0x7f040004;
+        public static final int username=0x7f040001;
     }
     public static final class style {
         /** 
--- src/kb50/appointment/LogIn.java
+++ src/kb50/appointment/LogIn.java
@@ -1,9 +1,9 @@
 package kb50.appointment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 public class LogIn extends Activity {
 
@@ -11,5 +11,10 @@ public class LogIn extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+    
+    public void onClick(View v){
+    	Intent i = new Intent(LogIn.this, TabLayout.class);
+    	startActivity(i);
     }
 }
