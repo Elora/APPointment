0e4516354bbcace3dfc29e8348d0a63f49adb559
0nenote <soufiane.s@gmail.com>
2015-01-08 15:09:34 +0100

    JSON parser, Appointments from DB.

--- src/kb50/appointment/Maps.java
+++ src/kb50/appointment/Maps.java
@@ -6,9 +6,6 @@ import android.app.Activity;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
